ed91c20bc6fc08e2cda03679964f49c6146065c4
Chad Wright <justdeejays@gmail.com>
2025-08-08 13:42:24 +0200

    fix: update devices sizes in next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    deviceSizes: [425, 800, 1280],
+    deviceSizes: [425, 800, 1280, 1600],
     minimumCacheTTL: 86400,
     remotePatterns: [
       {
